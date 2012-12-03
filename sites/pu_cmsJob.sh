75be8fae093f3a74d298c430d26e52fbd1cd514d
Andrzej Zuranski <zuranski@sesame2.(none)>
2012-12-03 14:29:21 -0500

    updates for pu jobs running faster

--- sites/pu_cmsJob.sh
+++ sites/pu_cmsJob.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-export SCRAM_ARCH=slc5_amd64_gcc434
-source /tigress-hsm/cmssoft/base5/cmsset_default.sh
-cd /tigress-hsm/cmssoft/base5/slc5_amd64_gcc434/cms/cmssw/CMSSW_5_0_0/src && eval `scram runtime -sh` && cd - >& /dev/null
+#export SCRAM_ARCH=slc5_amd64_gcc434
+#source /tigress-hsm/cmssoft/base5/cmsset_default.sh
+#cd /tigress-hsm/cmssoft/base5/slc5_amd64_gcc434/cms/cmssw/CMSSW_5_0_0/src && eval `scram runtime -sh` && cd - >& /dev/null
+export PATH=/home/zuranski/soft/Python-2.7.3/bin:$PATH
+export LD_LIBRARY_PATH=/home/zuranski/soft/Python-2.7.3/lib:$LD_LIBRARY_PATH
+. /home/zuranski/soft/root/bin/thisroot.sh
+
